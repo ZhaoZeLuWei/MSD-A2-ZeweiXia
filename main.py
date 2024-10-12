4995f59861d894bf96ce61ad82dc9ebc73a5a338
Zewei Xia <xiazeweibobby@gmail.com>
2024-10-13 01:16:43 +0800

    Optimizing the person class
    Adding Getter and Setter Methods
    Modify the property to be protected
    Optimize returned content

--- main.py
+++ main.py
@@ -4,16 +4,53 @@ def hello_A2():
     initiationString = "This is " + myName + "'s A2 github project "
     print(initiationString)
 
+#This class is an basic person class
 class person:
-    def __init__(self, firstName, lastName, age, id):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.age = age
-        self.id = id
+    def __init__(self, firstName = None, lastName = None, age = 0, id = 0):
+        self.firstName_ = firstName
+        self.lastName_ = lastName
+        self.age_ = age
+        self.id_ = id
 
-    def printPerson(self):
-        print(self.firstName + " " + self.lastName + " " + self.age + " " + self.id )
+    #getter and setter for each attribute
+    @property
+    def firstName(self):
+        return self.firstName_
 
+    @property
+    def lastName(self):
+        return self.lastName_
+
+    @property
+    def age(self):
+        return self.age_
+
+    @property
+    def id(self):
+        return self.id_
+
+    @firstName.setter
+    def firstName(self, value):
+        self.firstName_ = value
+
+    @lastName.setter
+    def lastName(self, value):
+        self.lastName_ = value
+
+    @age.setter
+    def age(self, value):
+        self.age_ = value
+
+    @id.setter
+    def id(self, value):
+        self.id_ = value
+
+    #return the person's information
+    def __str__(self):
+        return self.firstName + " " + self.lastName + " " + self.age + " " + self.id
+
+
+#This class is an basic doctor information class
 class Doctor:
     def __init__(self, firstName, lastName, age, job):
         self.firstName = firstName
