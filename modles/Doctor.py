2e8963c957cfcf5ec2740c9149a9c46cbbc9254f
Zewei Xia <xiazeweibobby@gmail.com>
2024-10-13 15:09:03 +0800

    Structure Management
    Break my code into a proper file structure for easier management

---
+++ modles/Doctor.py
@@ -0,0 +1,20 @@
+import Person
+
+class Doctor(Person):
+    def __init__(self, job):
+        super().__init__(self.firstName, self.lastName, self.age, self.id)
+        self.job_ = job
+
+    #getter and setter for each attribute
+    @property
+    def job(self):
+        return self.job_
+
+    @job.setter
+    def job(self, newJob):
+        if not isinstance(newJob, str):
+            raise ValueError("job must be a string")
+        self.job_ = newJob
+
+    def __str__(self):
+        return self.firstName + " " + self.lastName + " " + self.age + " " + self.job
---
+++ modles/Person.py
@@ -0,0 +1,51 @@
+class Person:
+    def __init__(self, firstName : str = "", lastName : str = "", age : int = 0, id : int = 0):
+        self.firstName_ = firstName
+        self.lastName_ = lastName
+        self.age_ = age
+        self.id_ = id
+
+    #getter and setter for each attribute
+    @property
+    def firstName(self):
+        return self.firstName_
+
+    @property
+    def lastName(self):
+        return self.lastName_
+
+    @property
+    def age(self):
+        return self.age_
+
+    @property
+    def id(self):
+        return self.id_
+
+    @firstName.setter
+    def firstName(self, value):
+        if not isinstance(value, str):
+            raise ValueError("firstName must be a string")
+        self.firstName_ = value
+
+    @lastName.setter
+    def lastName(self, value):
+        if not isinstance(value, str):
+            raise ValueError("lastName must be a string")
+        self.lastName_ = value
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise ValueError("age must be a positive number")
+        self.age_ = value
+
+    @id.setter
+    def id(self, value):
+        if not isinstance (value, int) or value < 0:
+            raise ValueError("id must be a positive number")
+        self.id_ = value
+
+    #return the person's information
+    def __str__(self):
+        return self.firstName + " " + self.lastName + " " + self.age + " " + self.id
